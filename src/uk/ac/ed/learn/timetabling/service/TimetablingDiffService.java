2290ec2b0754249de2df0513934410bd52b7e182
J Ross Nicoll <jrn@jrn.me.uk>
2013-05-01 12:36:50 +0000

    Added timetabling difference generation service.

---
+++ src/uk/ac/ed/learn/timetabling/service/TimetablingDiffService.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.learn.timetabling.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TimetablingDiffService extends Object {
+
+}
