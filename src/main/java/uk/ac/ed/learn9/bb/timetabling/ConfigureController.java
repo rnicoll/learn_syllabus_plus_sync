19c1a2b8c46ed8de942aa0086af8da9933ecbe8f
J Ross Nicoll <jrn@jrn.me.uk>
2013-05-07 14:10:53 +0000

    Reworked servlet mappings in Spring configuration to be simpler.

--- src/main/java/uk/ac/ed/learn9/bb/timetabling/ConfigureController.java
+++ src/main/java/uk/ac/ed/learn9/bb/timetabling/ConfigureController.java
@@ -2,8 +2,6 @@ package uk.ac.ed.learn9.bb.timetabling;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import blackboard.platform.context.Context;
-import blackboard.platform.context.ContextManagerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,7 +14,6 @@ public class ConfigureController {
      */
     @RequestMapping("/configure")
     public ModelAndView getConfigure(final HttpServletRequest request, final HttpServletResponse response) {
-        final Context context = ContextManagerFactory.getInstance().getContext();
         final ModelAndView modelAndView = new ModelAndView("configure");
         
         return modelAndView;
