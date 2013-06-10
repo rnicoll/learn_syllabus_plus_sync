12b331f1f507630d4c4771de1a89d568eceb9dbf
J Ross Nicoll <jrn@jrn.me.uk>
2013-06-10 13:34:46 +0000

    Added meaningful constraint names for Learn/Timetabling table PKs.

--- db/1 create tables.sql
+++ db/1 create tables.sql
@@ -3,14 +3,14 @@ CREATE TABLE activity_template (
   tt_template_name NVARCHAR2(255) DEFAULT NULL,
   tt_user_text_5 NVARCHAR2(255) DEFAULT NULL,
   learn_group_set_id VARCHAR2(80) DEFAULT NULL,
-  PRIMARY KEY (tt_template_id)
-);
+  constraint "ACTIVITY_TEMPLATE_PK" PRIMARY KEY (tt_template_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE activity_type (
   tt_type_id VARCHAR2(32) NOT NULL,
   tt_type_name VARCHAR2(255) DEFAULT NULL,
   PRIMARY KEY (tt_type_id)
-);
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE module (
   tt_module_id VARCHAR2(32) NOT NULL,
@@ -25,8 +25,8 @@ CREATE TABLE module (
   learn_course_code VARCHAR2(40) DEFAULT NULL,
   learn_course_id VARCHAR2(80) DEFAULT NULL,
   webct_active CHAR(1) DEFAULT NULL,
-  PRIMARY KEY (tt_module_id)  
-);
+  constraint "MODULE_PK" PRIMARY KEY (tt_module_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE activity (
   tt_activity_id VARCHAR2(32) NOT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE activity (
   learn_group_name NVARCHAR2(255) DEFAULT NULL,
   learn_group_created DATE DEFAULT NULL,
   description NVARCHAR2(2000) DEFAULT NULL,
-  PRIMARY KEY (tt_activity_id)
-);
+  constraint "ACTIVITY_PK" PRIMARY KEY (tt_activity_id)
+) tablespace "SATVLE_DATA";
 
 /* There's no referential integrity constraints here due to quality (or lack thereof)
  * of the source data.
@@ -50,8 +50,8 @@ CREATE TABLE activity_parents (
     tt_parent_activity_id VARCHAR2(32) NOT NULL,
     tt_obsolete_from INTEGER DEFAULT NULL,
     tt_latest_transaction INTEGER DEFAULT NULL,
-    PRIMARY KEY (tt_activity_id)
-);
+    constraint "ACTIVITY_PARENT_PK" PRIMARY KEY (tt_activity_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE variantjtaacts (
     tt_activity_id VARCHAR2(32) NOT NULL CONSTRAINT variant_activity REFERENCES activity (tt_activity_id),
@@ -60,13 +60,13 @@ CREATE TABLE variantjtaacts (
     tt_is_variant_parent NUMBER(3,0) NOT NULL,
     tt_is_variant_child NUMBER(3,0) NOT NULL,
     tt_latest_transaction INTEGER DEFAULT NULL
-);
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE run_result (
   result_code VARCHAR2(20) NOT NULL,
   result_label NVARCHAR2(80) NOT NULL,
-  PRIMARY KEY (result_code)
-);
+  constraint "RUN_RESULT_PK" PRIMARY KEY (result_code)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE synchronisation_run (
   run_id INTEGER NOT NULL,
@@ -75,41 +75,41 @@ CREATE TABLE synchronisation_run (
   diff_completed DATE DEFAULT NULL,
   end_time DATE DEFAULT NULL,
   result_code VARCHAR2(20) DEFAULT NULL REFERENCES run_result(result_code),
-  PRIMARY KEY (run_id)
-);
+  constraint "SYNCHRONISATION_RUN_PK" PRIMARY KEY (run_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE synchronisation_run_prev (
   run_id INTEGER NOT NULL REFERENCES synchronisation_run(run_id),
   previous_run_id INTEGER NULL REFERENCES synchronisation_run(run_id),
-  PRIMARY KEY(run_id),
-  UNIQUE(previous_run_id)
-);
+  constraint "SYNC_RUN_PREV_PK" PRIMARY KEY(run_id),
+  constraint "SYNC_RUN_PREV_UNIQ" UNIQUE(previous_run_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE cache_enrolment (
   run_id INTEGER NOT NULL CONSTRAINT cache_run REFERENCES synchronisation_run(run_id),
   tt_student_set_id VARCHAR2(32) NOT NULL,
   tt_activity_id VARCHAR2(32) NOT NULL CONSTRAINT cache_activ REFERENCES activity (tt_activity_id),
-  PRIMARY KEY (run_id,tt_student_set_id,tt_activity_id)
-);
+  constraint "CACHE_ENROLMENT_PK" PRIMARY KEY (run_id,tt_student_set_id,tt_activity_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE change_result (
   result_code VARCHAR2(20) NOT NULL,
   label NVARCHAR2(80) NOT NULL,
   retry NUMBER(1) DEFAULT '0' NOT NULL,
-  PRIMARY KEY (result_code)
-);
+  constraint "CHANGE_RESULT_PK" PRIMARY KEY (result_code)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE change_type (
   change_type VARCHAR2(12) NOT NULL,
-  PRIMARY KEY (change_type)
-);
+  constraint "CHANGE_TYPE_PK" PRIMARY KEY (change_type)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE student_set (
   tt_student_set_id VARCHAR2(32) NOT NULL,
   tt_host_key VARCHAR2(32) NOT NULL,
   learn_user_id VARCHAR2(80) DEFAULT NULL,
-  PRIMARY KEY (tt_student_set_id)
-);
+  constraint "STUDENT_SET_PK" PRIMARY KEY (tt_student_set_id)
+) tablespace "SATVLE_DATA";
 
 CREATE TABLE enrolment_change (
   change_id INTEGER NOT NULL,
@@ -119,5 +119,5 @@ CREATE TABLE enrolment_change (
   change_type VARCHAR2(12) NOT NULL CONSTRAINT enrol_change_type REFERENCES change_type(change_type),
   result_code VARCHAR2(20) DEFAULT NULL CONSTRAINT enrol_change_result REFERENCES change_result(result_code),
   update_completed DATE DEFAULT NULL,
-  PRIMARY KEY (change_id)
-);
+  constraint "ENROLMENT_CHANGE_PK" PRIMARY KEY (change_id)
+) tablespace "SATVLE_DATA";
