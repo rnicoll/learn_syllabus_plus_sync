d933c07f9be1c8ce8b7eb9c5fac8fbf70cd2d075
J Ross Nicoll <jrn@jrn.me.uk>
2013-06-10 14:50:31 +0000

    Added method to refresh a SynchronisationRun from the database.

--- src/main/java/uk/ac/ed/learn9/bb/timetabling/dao/SynchronisationRunDao.java
+++ src/main/java/uk/ac/ed/learn9/bb/timetabling/dao/SynchronisationRunDao.java
@@ -1,6 +1,7 @@
 package uk.ac.ed.learn9.bb.timetabling.dao;
 
 import java.util.Collection;
+
 import uk.ac.ed.learn9.bb.timetabling.data.SynchronisationRun;
 
 /**
@@ -17,4 +18,10 @@ public interface SynchronisationRunDao {
      * @return the synchronisation run.
      */
     public SynchronisationRun getRun(int runId);
+
+    /**
+     * Refresh the synchronisation run from the database.
+     * @param run the synchronisation run to be refreshed.
+     */
+    public void refresh(SynchronisationRun run);
 }
--- src/main/java/uk/ac/ed/learn9/bb/timetabling/dao/impl/SynchronisationRunDaoImpl.java
+++ src/main/java/uk/ac/ed/learn9/bb/timetabling/dao/impl/SynchronisationRunDaoImpl.java
@@ -1,13 +1,13 @@
 package uk.ac.ed.learn9.bb.timetabling.dao.impl;
 
 import java.util.List;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ed.learn9.bb.timetabling.dao.SynchronisationRunDao;
-import uk.ac.ed.learn9.bb.timetabling.data.SynchronisationRun;
 import uk.ac.ed.learn9.bb.timetabling.data.SynchronisationRun;
 
 /**
@@ -25,6 +25,11 @@ public class SynchronisationRunDaoImpl extends HibernateDaoSupport implements Sy
     @Override
     public SynchronisationRun getRun(int runId) {
         return (SynchronisationRun)this.getSession().get(SynchronisationRun.class, runId);
+    }
+
+    @Override
+    public void refresh(SynchronisationRun run) {
+        this.getSession().refresh(run);
     }
     
 }
