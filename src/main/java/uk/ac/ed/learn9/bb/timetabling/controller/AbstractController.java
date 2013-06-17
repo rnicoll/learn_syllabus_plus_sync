f90aa6f2cab2a0626894c5a48c6463ad46c51087
J Ross Nicoll <jrn@jrn.me.uk>
2013-06-17 13:34:29 +0000

    Added AbstractController class.

---
+++ src/main/java/uk/ac/ed/learn9/bb/timetabling/controller/AbstractController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.ed.learn9.bb.timetabling.controller;
+
+/**
+ *
+ * @author jnicoll2
+ */
+public abstract class AbstractController {
+    public static final String PLUGIN_VENDOR_ID = "uoe";
+    public static final String PLUGIN_ID = "plgn-timetabling";
+    
+    public static final String US_ASCII = "US-ASCII";
+}
