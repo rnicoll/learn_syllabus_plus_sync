1a5403ac14b5359ed35b6409a4c531cadd76bc5a
J Ross Nicoll <jrn@jrn.me.uk>
2013-07-09 13:51:13 +0000

    Started adding mock classes for loading Blackboard data.

---
+++ src/test/java/uk/ac/ed/learn9/bb/timetabling/blackboard/CourseMockLoader.java
@@ -0,0 +1,281 @@
+package uk.ac.ed.learn9.bb.timetabling.blackboard;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import blackboard.base.AppVersion;
+import blackboard.base.BbList;
+import blackboard.data.course.Course;
+import blackboard.data.course.Course.ServiceLevel;
+import blackboard.data.course.CourseMembership.Role;
+import blackboard.persist.BbPersistenceManager;
+import blackboard.persist.Id;
+import blackboard.persist.KeyNotFoundException;
+import blackboard.persist.PersistenceException;
+import blackboard.persist.course.CourseDbLoader;
+import blackboard.persist.course.CourseSearch;
+
+/**
+ * Mock user loading class for use in automated testing.
+ */
+public class CourseMockLoader implements CourseDbLoader {
+    private AppVersion appVersion;
+    private final Map<Id, Course> courseById = new HashMap<Id, Course>();
+    private final Map<String, Course> courseByCourseId = new HashMap<String, Course>();
+    private Course systemCourse = null;
+    
+    /**
+     * Add a course to the mock loader.
+     * 
+     * @param course course to be stored ready for loading.
+     */
+    public void addCourse(final Course course) {
+        assert null != course;
+        assert null != course.getId();
+        assert null != course.getCourseId();
+        
+        this.courseById.put(course.getId(), course);
+        this.courseByCourseId.put(course.getCourseId(), course);
+    }
+    
+    public void setSystemCourse(final Course newSystemCourse) {
+        this.systemCourse = newSystemCourse;
+    }
+ 
+    @Override
+    public Course loadById(Id id) throws KeyNotFoundException, PersistenceException {
+        final Course course = this.courseById.get(id);
+        
+        if (null == course) {
+            throw new KeyNotFoundException("Could not find course \""
+                + id.getExternalString() + "\".");
+        } else {
+            return course;
+        }
+    }
+
+    @Override
+    public Course loadById(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadById(id);
+    }
+
+    @Override
+    public Course loadById(Id id, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        return this.loadById(id);
+    }
+
+    @Override
+    public Course loadByCourseId(final String courseId) throws KeyNotFoundException, PersistenceException {
+        final Course course = this.courseByCourseId.get(courseId);
+        
+        if (null == course) {
+            throw new KeyNotFoundException("Could not find course \""
+                + courseId + "\".");
+        } else {
+            return course;
+        }
+    }
+
+    @Override
+    public boolean doesCourseIdExist(final String courseId) {
+        return this.courseByCourseId.containsKey(courseId);
+    }
+
+    @Override
+    public boolean doesCourseIdExist(String string, Connection cnctn) {
+        return this.doesCourseIdExist(string);
+    }
+
+    @Override
+    public Course loadByCourseId(String courseId, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByCourseId(courseId);
+    }
+
+    @Override
+    public Course loadByCourseId(String courseId, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        return this.loadByCourseId(courseId);
+    }
+
+    @Override
+    public BbList<Course> loadByUserId(final Id userId) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserId(final Id userId, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserId(final Id userId, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndSortColumns(Id id, List<String> list, ServiceLevel sl) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndSortColumns(Id id, List<String> list, ServiceLevel sl, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndDirectEnrollments(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndDirectEnrollments(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByServiceLevelAndRoles(Id id, ServiceLevel sl, List<Role> list) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndCourseMembershipRole(Id id, Role role) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndCourseMembershipRole(Id id, Role role, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdAndCourseMembershipRole(Id id, Role role, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdForLearners(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdForLearners(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByUserIdForLearners(Id id, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Course loadByBatchUid(final String batchUid) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Course loadByBatchUid(final String batchUid, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Course loadByBatchUid(final String batchUid, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByDataSourceBatchUid(final String batchUid) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByDataSourceBatchUid(final String batchUid, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadAllCourses() throws KeyNotFoundException, PersistenceException {
+        return new BbList<Course>(this.courseById.values());
+    }
+
+    @Override
+    public BbList<Course> loadAllCourses(Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadAllCourses();
+    }
+
+    @Override
+    public BbList<Course> loadAllByServiceLevel(ServiceLevel sl) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadAllByServiceLevel(ServiceLevel sl, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadAllByServiceLevel(sl);
+    }
+
+    @Override
+    public Course loadSystemCourse() throws KeyNotFoundException, PersistenceException {
+        return this.systemCourse;
+    }
+
+    @Override
+    public Course loadSystemCourse(Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadSystemCourse();
+    }
+
+    @Override
+    public BbList<Course> loadCourseAndInstructorListByServiceLevel(ServiceLevel sl) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadCourseAndInstructorListByServiceLevel(ServiceLevel sl, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadCourseAndInstructorListByServiceLevel(sl);
+    }
+
+    @Override
+    public BbList<Course> loadTemplates() throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadTemplates(Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadTemplates();
+    }
+
+    @Override
+    public List<Course> loadByCourseSearch(CourseSearch cs) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByCourseCategoryId(Id id) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByCourseCategoryId(Id id, Connection cnctn) throws PersistenceException {
+        return this.loadByCourseCategoryId(id);
+    }
+
+    @Override
+    public BbList<Course> loadByOrgCategoryId(Id id) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<Course> loadByOrgCategoryId(Id id, Connection cnctn) throws PersistenceException {
+        return this.loadByOrgCategoryId(id);
+    }
+
+    @Override
+    public void init(BbPersistenceManager bpm, final AppVersion av) {
+        this.persistenceManager = bpm;
+        this.appVersion = av;
+    }
+
+    @Override
+    public AppVersion getAppVersion() {
+        return this.appVersion;
+    }
+}
---
+++ src/test/java/uk/ac/ed/learn9/bb/timetabling/blackboard/UserMockLoader.java
@@ -0,0 +1,242 @@
+package uk.ac.ed.learn9.bb.timetabling.blackboard;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import blackboard.base.AppVersion;
+import blackboard.base.BbList;
+import blackboard.data.user.User;
+import blackboard.persist.BbPersistenceManager;
+import blackboard.persist.Id;
+import blackboard.persist.KeyNotFoundException;
+import blackboard.persist.PersistenceException;
+import blackboard.persist.impl.mapping.DbObjectMap;
+import blackboard.persist.user.UserDbLoader;
+import blackboard.persist.user.UserSearch;
+import blackboard.persist.user.UserSearch.SearchParameter;
+
+/**
+ * Mock user loading class for use in automated testing.
+ */
+public class UserMockLoader implements UserDbLoader {
+    private AppVersion appVersion;
+    private final Map<Id, User> userById = new HashMap<Id, User>();
+    private final Map<String, User> userByUsername = new HashMap<String, User>();
+    private User guestUser;
+    private BbPersistenceManager persistenceManager;
+    
+    /**
+     * Add a user to the mock loader.
+     * 
+     * @param user user to be stored ready for loading.
+     */
+    public void addUser(final User user) {
+        assert null != user;
+        assert null != user.getId();
+        assert null != user.getUserName();
+        
+        this.userById.put(user.getId(), user);
+        this.userByUsername.put(user.getUserName(), user);
+    }
+    
+    public void setGuestUser(final User user) {
+        this.guestUser = user;
+    }
+
+    @Override
+    public User loadById(Id id) throws KeyNotFoundException, PersistenceException {
+        final User user = this.userById.get(id);
+        
+        if (null == user) {
+            throw new KeyNotFoundException("Could not find user \""
+                + id.getExternalString() + "\".");
+        } else {
+            return user;
+        }
+    }
+
+    @Override
+    public User loadById(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadById(id);
+    }
+
+    @Override
+    public User loadById(Id id, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        return this.loadById(id);
+    }
+
+    @Override
+    public User loadById(Id id, Connection cnctn, boolean bln, DbObjectMap dom) throws KeyNotFoundException, PersistenceException {
+        return this.loadById(id);
+    }
+
+    @Override
+    public BbList<User> loadByEmailAddressFamilyNameGivenName(final String emailAddress, final String familyName, final String givenName) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadByEmailAddressFamilyNameGivenName(final String emailAddress, final String familyName, final String givenName, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByEmailAddressFamilyNameGivenName(emailAddress, familyName, givenName);
+    }
+
+    @Override
+    public BbList<User> loadByStudentIdFamilyNameGivenName(final String studentId, final String familyName, final String givenName, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadByCardNumberFamilyNameGivenName(final String cardNumber, final String familyName, final String givenName, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> searchByUserName(final String username) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadByUserName(final String username) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadByUserName(final String username, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        final User user = this.userByUsername.get(username);
+        
+        if (null == user) {
+            throw new KeyNotFoundException("Could not find user \""
+                + username + "\".");
+        } else {
+            return user;
+        }
+    }
+
+    @Override
+    public User loadByUserName(final String username, Connection cnctn, boolean bln) throws KeyNotFoundException, PersistenceException {
+        return this.loadByUserName(username);
+    }
+
+    @Override
+    public BbList<User> loadByCourseId(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadByCourseId(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByCourseId(id);
+    }
+
+    @Override
+    public BbList<User> loadByCourseId(Id id, Connection cnctn, boolean bln) throws KeyNotFoundException, PersistenceException {
+        return this.loadByCourseId(id);
+    }
+
+    @Override
+    public BbList<User> loadByGroupId(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadByGroupId(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByGroupId(id);
+    }
+
+    @Override
+    public BbList<User> loadByGroupId(Id id, Connection cnctn, boolean bln) throws KeyNotFoundException, PersistenceException {
+        return this.loadByGroupId(id);
+    }
+
+    @Override
+    public User loadByBatchUid(final String batchUid) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadByBatchUid(final String batchUid, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByBatchUid(batchUid);
+    }
+
+    @Override
+    public User loadByBatchUid(final String batchUid, Connection cnctn, boolean bln) throws KeyNotFoundException, PersistenceException {
+        return this.loadByBatchUid(batchUid);
+    }
+
+    @Override
+    public BbList<User> loadObservedByObserverId(final Id observerId) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadObservedByObserverId(final Id observerId, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadObservedByObserverId(observerId);
+    }
+
+    @Override
+    public BbList<User> loadObservedByObserverId(final Id observerId, Connection cnctn, boolean bHeavy) throws KeyNotFoundException, PersistenceException {
+        return this.loadObservedByObserverId(observerId);
+    }
+
+    @Override
+    public BbList<User> loadByPrimaryPortalRoleId(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BbList<User> loadByPrimaryPortalRoleId(Id id, Connection cnctn) throws KeyNotFoundException, PersistenceException {
+        return this.loadByPrimaryPortalRoleId(id);
+    }
+
+    @Override
+    public BbList<User> loadByPrimaryPortalRoleId(Id id, Connection cnctn, boolean bln) throws KeyNotFoundException, PersistenceException {
+        return this.loadByPrimaryPortalRoleId(id);
+    }
+
+    @Override
+    public List<User> loadByPortalRoleId(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadUserByUserRole(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadUserByCourseMembership(Id id) throws KeyNotFoundException, PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<User> loadByUserSearch(UserSearch us) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User loadGuestUser() throws KeyNotFoundException, PersistenceException {
+        return this.guestUser;
+    }
+
+    @Override
+    public List<User> loadUsersForUserDirectory(DbObjectMap dom, SearchParameter sp) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<User> loadUsersForUserDirectory(DbObjectMap dom, SearchParameter sp, Connection cnctn) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void init(BbPersistenceManager bpm, AppVersion av) {
+        this.persistenceManager = bpm;
+        this.appVersion = av;
+    }
+
+    @Override
+    public AppVersion getAppVersion() {
+        return this.appVersion;
+    }
+}
