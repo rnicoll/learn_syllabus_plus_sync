4167ac7e2b10dec73bb913a84d46f14223ed2734
J Ross Nicoll <jrn@jrn.me.uk>
2013-08-14 13:05:48 +0000

    STU226-139: Added patch file to remove excess records for module_courses.

---
+++ db/Patches/STU226-139.sql
@@ -0,0 +1,5 @@
+DELETE FROM SATVLE.MODULE_COURSE WHERE MODULE_COURSE_ID IN (SELECT A.MODULE_COURSE_ID FROM SATVLE.module_CoursE A
+  JOIN SATVLE.MODULE_COURSE B ON B.MODULE_COURSE_ID<A.MODULE_COURSE_ID AND
+    a.tt_module_id=b.tt_module_id AND a.learn_course_code=B.learn_course_code
+  WHERE a.module_course_id NOT IN (SELECT module_course_id FROM SATVLE.ACTIVITY_GROUP));
+  
