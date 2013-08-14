26e01afdf9bd49cb1913a7a011ce1932d950fb0a
J Ross Nicoll <jrn@jrn.me.uk>
2013-08-14 10:38:48 +0000

    STU226-134: Added missing Javadoc to patch file.

--- db/Patches/STU226-134.sql
+++ db/Patches/STU226-134.sql
@@ -1,2 +1,8 @@
-
+/**
+ * Drop the constraint which stops multiple synchronisation runs from
+ * generating differences against a single run, as this causes problems
+ * where runs fail and are unsafe to generate differences again, but "lock"
+ * the run they are based on.
+ */
+ 
 ALTER TABLE SYNCHRONISATION_RUN_PREV DROP CONSTRAINT SYNC_RUN_PREV_UNIQ;
