32f990855da95ed9b62cdaa37c257a446c012332
J Ross Nicoll <jrn@jrn.me.uk>
2013-08-15 09:53:45 +0000

    STU226-144: Further SQL for cleaning up duplicate groups in Learn.

--- db/Patches/STU226-139.sql
+++ db/Patches/STU226-139.sql
@@ -1,5 +1,6 @@
 DELETE FROM MODULE_COURSE WHERE MODULE_COURSE_ID IN (SELECT A.MODULE_COURSE_ID FROM module_course A
   JOIN MODULE_COURSE B ON B.MODULE_COURSE_ID<A.MODULE_COURSE_ID AND
     a.tt_module_id=b.tt_module_id AND a.learn_course_code=B.learn_course_code
-  WHERE a.module_course_id NOT IN (SELECT module_course_id FROM SATVLE.ACTIVITY_GROUP));
+  WHERE a.module_course_id NOT IN (SELECT module_course_id FROM ACTIVITY_GROUP)
+    AND a.module_course_id NOT IN (SELECT module_course_id FROM ENROLMENT_CHANGE_PART));
   
